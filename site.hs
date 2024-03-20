b70a0d6c98b113cc5ff52a26360679297ed64e75
frankt716 <dailyuse716@gmail.com>
2024-03-20 16:14:30 -0400

    implemented post

--- site.hs
+++ site.hs
@@ -1,8 +1,10 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import Data.Monoid (mappend)
+import Data.Monoid     (mappend)
 import Hakyll
+import Data.List       (stripPrefix)
 import System.Process
+import System.FilePath (takeBaseName, (</>))
 --------------------------------------------------------------------------------
 config :: Configuration
 config = defaultConfiguration
@@ -11,7 +13,7 @@ config = defaultConfiguration
 
 coqdocCompiler :: Compiler (Item String)
 coqdocCompiler = do
-  coq_filename <- toFilePath <$> getUnderlying
+  coqFilename <- toFilePath <$> getUnderlying
   body <- unsafeCompiler $
     readProcess "coqdoc" [ "--no-index"
                          , "--stdout"
@@ -19,9 +21,25 @@ coqdocCompiler = do
                          , "--parse-comments"
                          , "-utf8"
                          , "-s"
-                         , coq_filename ] ""
+                         , coqFilename ] ""
   makeItem body
 
+coqPostCompiler :: Compiler (Item String)
+coqPostCompiler = do
+  id <- getUnderlying
+  route <- getRoute id
+  coqFilename <- getMetadataField id "coqfile"
+  case coqFilename of
+    Just coqFilename ->
+      let fullName = "rsc" </> coqFilename
+          baseName = takeBaseName coqFilename in do
+        postBody <- loadBody $ fromFilePath fullName
+        makeItem $ flip withUrls postBody $ \url ->
+          case (stripPrefix (baseName ++ ".html") url, route) of
+            (Just url', Just route) -> toUrl $ route ++ url'
+            _ -> url
+    Nothing -> error "Missing metadata \"coqfile\""
+--------------------------------------------------------------------------------
 main :: IO ()
 main = hakyllWith config $ do
   match "images/*" $ do
@@ -33,40 +51,41 @@ main = hakyllWith config $ do
     compile compressCssCompiler
 
   match "rsc/*.v" $ do
-    route $ setExtension "html"
     compile $ coqdocCompiler
+
+  match "posts/*.coqpost" $ do
+    route $ setExtension "html"
+    compile $ coqPostCompiler
       >>= loadAndApplyTemplate "templates/coqdoc.html" defaultContext
       >>= relativizeUrls
 
-  match "posts/*" $ do
-    route $ setExtension "html"
-    compile $ pandocCompiler
-      >>= loadAndApplyTemplate "templates/post.html"    postCtx
-      >>= loadAndApplyTemplate "templates/default.html" postCtx
-      >>= relativizeUrls
+  -- match "posts/*" $ do
+  --   route $ setExtension "html"
+  --   compile $ pandocCompiler
+  --     >>= loadAndApplyTemplate "templates/post.html"    postCtx
+  --     >>= loadAndApplyTemplate "templates/default.html" postCtx
+  --     >>= relativizeUrls
 
-  create ["archive.html"] $ do
-    route idRoute
-    compile $ do
-      posts <- recentFirst =<< loadAll "posts/*"
-      let archiveCtx =
-            listField "posts" postCtx (return posts) `mappend`
-            constField "title" "Archives"            `mappend`
-            defaultContext
+  -- create ["archive.html"] $ do
+  --   route idRoute
+  --   compile $ do
+  --     posts <- recentFirst =<< loadAll "posts/*"
+  --     let archiveCtx =
+  --           listField "posts" postCtx (return posts) `mappend`
+  --           constField "title" "Archives"            `mappend`
+  --           defaultContext
 
-      makeItem ""
-        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-        >>= relativizeUrls
+  --     makeItem ""
+  --       >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+  --       >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+  --       >>= relativizeUrls
 
   match "index.html" $ do
     route idRoute
     compile $ do
       posts <- recentFirst =<< loadAll "posts/*"
-      rscs <- loadAll "rsc/*"
       let indexCtx =
             listField "posts" postCtx (return posts)       `mappend`
-            listField "rscs"  defaultContext (return rscs) `mappend`
             defaultContext
 
       getResourceBody
